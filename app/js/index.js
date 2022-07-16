d0a5256489ce6619b48a7190cdcb6cacf9e29dbc
Filippo95 <filippo.luppi@gmail.com>
2022-07-16 14:49:36 +0200

    alerting

--- app/js/index.js
+++ app/js/index.js
@@ -16,10 +16,20 @@ scanButton.addEventListener("click", async () => {
             console.log(`> Serial Number: ${serialNumber}`);
             console.log(serialNumber.replaceAll(":",""));
             localStorage.setItem("user_skill_card_serial",serialNumber.replaceAll(":",""));
-            $("#alert").innerHTML("<div class=\"alert alert-info\">" +
-                "                    <h4 class=\"alert-heading\">Lettura NFC Effettuata Correttamente ... attendere</h4>" +
-                "                    <p>La lettura del della Skill Card è stata eseguita con successo, attendere fino al completamento della procedura di autenticazione</p>" +
-                "                </div>");
+            if(localStorage.getItem("user_skill_card_serial")){
+                $("#alert").innerHTML("<div class=\"alert alert-info\">" +
+                    "                    <h4 class=\"alert-heading\">Lettura NFC Effettuata Correttamente ... attendere</h4>" +
+                    "                    <p>La lettura del della Skill Card è stata eseguita con successo, attendere fino al completamento della procedura di autenticazione</p>" +
+                    "                </div>")
+            }
+            else{
+                $("#alert").innerHTML("<div class=\"alert alert-primary\" style=\"visibility: hidden\">\n" +
+                    "                    <h4 class=\"alert-heading\">Errore nella Lettura NFC</h4>\n" +
+                    "                    <p>Si è verificato un errore nella lettura della Skill Card</p>\n" +
+                    "                    <p id=\"error\"></p>\n" +
+                    "                </div>")
+            }
+
 
         });
     } catch (error) {
